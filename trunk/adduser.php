e18e71f736d1ea964e02177a8f7716bcaa525f7b
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2010-09-10 13:41:10 +0000

    Initial Upload

---
+++ trunk/adduser.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+<form method="post" action="newplayer.php">
+<span class="text-normal">Name: <input type="text" name="name" size="25" maxlength="50" /><br />
+Phone: <input type="text" name="phone" size="25" maxlength="50" /><br />
+Mobile: <input type="text" name="mobilephone" size="25" maxlength="50" /><br />
+Email: <input type="text" name="email" size="25" maxlength="50" /><br /></span>
+
+<input type="submit" />
+</form>
+</body>
+</html>
