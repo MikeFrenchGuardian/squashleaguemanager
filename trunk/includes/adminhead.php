a8ad03be9f24479991e21da8454e59bffc82c05a
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2010-09-11 15:16:17 +0000

    fixed admin links further

---
+++ trunk/includes/adminhead.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>TomJohn League</title>
+<link rel="stylesheet" type="text/css" href="../style.css" />
+</head>
+<body class="text-normal" bgcolor="#C0C0C0" padding="0">
+<?php
+require_once '../login.php';
+require_once '../dbCheck.php';
+require_once '../functions.php';
+?>
+<div class="content">
+
+<div class="cruising">
+<span class="logo-white">TomJohn </span><span class="logo-red"> League</span>
+
+	
+</div>
+<div class="text-area">
