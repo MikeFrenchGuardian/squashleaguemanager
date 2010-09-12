0f1f51a103744be9a7b2c9f11b90102d2f6f70c9
nick <nick@nickwales.co.uk>
2010-09-12 07:20:16 +0000

    added division ordering and dropdowns

--- admin/setupDivision.php
+++ admin/setupDivision.php
@@ -32,14 +32,14 @@ echo "<br>";
 
 
 $seasonID = $_GET["season"];
-
+$seasonID = 3;
 if (isset($seasonID)) {
 	$seasonID = $seasonID;
 }	else {
 	$seasonID = currentSeason();
 }
 
-
+$leagueCount = numLeagues($seasonID);
 
 
 for ($i = 1; $i <= 4; ++$i) {
@@ -59,6 +59,7 @@ for ($i = 1; $i <= 4; ++$i) {
 			$playerID = getDivPlayers($i,$seasonID,$j);
 			$playerName = getPlayerName($playerID);
 			$playerLeague = getPlayerLeague($playerID,$seasonID);
+			$points = getLeaguePoints($playerID,$seasonID);
 			$arrayNo = "player" .$j;
 
 	
@@ -67,6 +68,7 @@ for ($i = 1; $i <= 4; ++$i) {
 						"playerID" => $playerID,
 						"player" => $playerName,
 						"league" => $playerLeague,
+						"points" => $points,
 				);	   
 
 			$leagueArray[] = $arrayNo;
@@ -78,7 +80,17 @@ foreach ($leagueArray as $position) {
 	echo	'<tr>';
 	echo	'<td><a href="playerdetail.php?id=' . $position['playerID'] . '" class="text-normal">' . $position['player'] . '</td>';
 	echo	'<td class="text-normal"><select selected="' . $position['league'] . '">';
-    echo "<option>1</option><option>2</option><option>3</option><option>4</option></select></td>";
+	for ($j = 1; $j <= $leagueCount ; ++$j ) {
+		if ($j==$i) {
+			echo "<option value=" . $j . " selected>" . $j . " </option>";
+		} else {
+			echo "<option value=" . $j . ">" . $j . " </option>";
+		}	
+ //   echo "<option>2</option>";
+ //   echo "<option>3</option>";
+ //   echo "<option>4</option>";
+	}
+    echo "</select></td>";
 	echo	'</tr>';
   }
 	echo "</table>  <br><br>";
