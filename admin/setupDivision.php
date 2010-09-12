a68b140c6479556bded917de9b4c626d1824912d
nick <nick@nickwales.co.uk>
2010-09-12 21:18:38 +0000

    adding players to div work started

--- admin/setupDivision.php
+++ admin/setupDivision.php
@@ -5,14 +5,18 @@
 if (isset($_POST['season'])) $seasonID = sanitizeString($_POST['season']);
 if (isset($_POST['leagueNum'])) $leagueNum = sanitizeString($_POST['leagueNum']);
 
+// If the page receives a create leagues request then we create them, otherwise we just add players to the league.
 
-//$seasonID = $_GET["season"];
-
-//if (isset($seasonID)) {
-//	$seasonID = $seasonID;
-//}	else {
-//	$seasonID = currentSeason();
-//}
+if (isset($seasonID)) {
+	//	$seasonID = $seasonID;
+	// Insert leagues into DB.
+	// Loop for each league to add them to the DB
+	for ($h = 1; $h <= $leagueNum; ++$h) {
+		createNewLeagues($seasonID,$h);
+	}
+} else {
+	$seasonID = currentSeason();
+}
 
 
 // Insert leagues into DB.
@@ -75,38 +79,42 @@ for ($i = 1; $i <= 4; ++$i) {
 			$leagueArray[] = $arrayNo;
 
 	}
+
 usort($leagueArray, "sortDescending");
 
+?>
+<form method="get" action="divsetup2.php">
+
+<?php
 // setup the leagues
 foreach ($leagueArray as $position) {
 	echo	'<tr>';
+	// Render PlayerName
 	echo	'<td><a href="playerdetail.php?id=' . $position['playerID'] . '" class="text-normal">' . $position['player'] . '</td>';
+	// Render Player Points
 	echo	'<td class="text-normal">' . $position['points'] . '</td>';
-	echo	'<td class="text-normal"><select selected="' . $position['league'] . '">';
-	// create the dynamic dropdowns 
+	// Render Division dropdown
+	echo	"<td class=\"text-normal\"><select name=\"" . $position['playerID'] . "\">";
+		// Run through number of leagues in the given season for each dropdown
 	for ($j = 1; $j <= $leagueCount ; ++$j ) {
 		if ($j==$i) {
-			echo "<option value=" . $j . " selected>" . $j . " </option>";
+			// Render the option name as the playerID
+			echo "<option name=\"" . $position['playerID'] ."\" value=\"" . $j . "\"selected>" . $j . "</option>";
 		} else {
-			echo "<option value=" . $j . ">" . $j . " </option>";
+			echo "<option name=\"" . $position['playerID'] ."\" value=\"" . $j . "\">" . $j . "</option>";
 		}	
 	}
     echo "</select></td>";
 	echo	'</tr>';
   }
 	echo "</table>  <br><br>";
-
-
 }
-?>
-<?php require_once '../includes/adminfooter.php'; ?>
 
 
 
 
+echo "<input type=\"submit\">";
+echo "</form>";
 
 
-
-
-
-
+ require_once '../includes/adminfooter.php'; ?>
