aaedced1a055da255784f70d2752e354c0d1028c
nick <nick@nickwales.co.uk>
2010-09-15 13:26:33 +0000

    added includes to the admin index page

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,5 @@
-<html>
-<head>
-<title>TomJohn League</title>
-<link rel="stylesheet" type="text/css" href="../style.css" />
-</head>
-<body class="text-normal" bgcolor="#C0C0C0" padding="0">
-<?php
-require_once '../login.php';
-require_once '../dbCheck.php';
-require_once '../functions.php';
-?>
-<div class="content">
+<?php require_once '../includes/adminhead.php'; ?>
 
-<div class="cruising">
-<span class="logo-white">TomJohn </span><span class="logo-red"> League</span>
-
-	
-</div>
-<div class="text-area">
 
 <span class="text-header">Administration Area</span><br><br>
 
