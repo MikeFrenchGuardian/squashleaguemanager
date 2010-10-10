e4cc069c147caafe5cdcb3f0ad2b90072ee5443f
nick <nick@nickwales.co.uk>
2010-10-10 12:06:56 +0000

    recent results moved to header

--- includes/head.php
+++ includes/head.php
@@ -12,6 +12,48 @@ require_once 'functions.php';
 <div class="content">
 
 <div class="cruising">
+
+<?php 
+
+
+// grab most recent 5 results from the db
+$query = "SELECT player1, player2, player1_score, player2_score FROM results"; // LIMIT 0,5";
+$result = mysql_query($query);
+$rows = mysql_num_rows($result);
+
+
+
+$last5 = $rows-5; //not required anymore... db going the other way?!?
+
+
+      
+?>
+<div class="recentResults">
+<span class="text-medium-header">Latest Results</span><br>
+<table class="stats">
+<tr>
+   <td class="hed">Winner</td>
+   <td class="hed">Runner-Up</td>
+
+</tr>
+<?php
+for ($j = $last5 ; $j < $rows ; ++$j)
+{
+        $player1 = mysql_result($result,$j,'player1');
+        $player2 = mysql_result($result,$j,'player2');
+        $p1_score = mysql_result($result,$j,'player1_score');
+        $p2_score = mysql_result($result,$j,'player2_score');
+
+
+echo    '<tr>';
+echo    '<td class="text-normal">' . getPlayerName($player1) . ' ' . $p1_score . '</td>';
+echo    '<td class="text-normal">' . $p2_score . ' ' . getPlayerName($player2) . '</td>';
+echo    '</tr>';
+}
+echo "</table> <br><br>";
+?>
+</div>
+
 <span class="logo-white">TomJohn </span><span class="logo-red"> League</span>
 
 	
