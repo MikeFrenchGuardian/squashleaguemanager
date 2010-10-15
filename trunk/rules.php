f43406f2a831027b9f8422fce0dfe2c6793c4a78
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2010-10-15 09:54:15 +0000

    cleaned up rules page

--- trunk/rules.php
+++ trunk/rules.php
@@ -6,14 +6,15 @@
 
 <br><span class="text-header">League Rules</span><br><br>
 
-Any result received later than noon of the final day will not be counted.<br>Two players will be promoted / demoted in each league.  This can change with players joining / leaving the leagues.<br>If any players gives away 2 walkovers or more in one session, all other results will be wiped out.<br><br>
-
-In the event of any queries or discontent please contact:	<br>
+<ul>
+	<li>Any result received later than noon of the final day will not be counted.<br>	<li>Two players will be promoted / demoted in each league.  This can change with players joining / leaving the leagues.<br>	<li>If any players gives away 2 walkovers or more in one session, all other results will be wiped out.<br><br>
+</ul>
+In the event of any queries or discontent please contact:<br>
 
 <ul>	<li>Simon Austen;<br>	<li>James Balfour; or<br>	<li>Odin Madsen.<br>
 </ul>	
 <br>
-Scoring	<br>	<table class="text-normal" left-padding="30">
+Scoring	<br>	<table class="league" left-padding="30">
 <tr><td>Result</td><td>Points</td></tr>
 <tr><td>3-0</td><td>7-1</td></tr>
 <tr><td>3-1</td><td>6-2</td></tr>
