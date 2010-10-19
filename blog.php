e37507bd54a84c2624193fb5f63eaafc658c3c37
nick <nick@nickwales.co.uk>
2010-10-19 10:44:26 +0000

    added synopsis to blog

---
+++ blog.php
@@ -0,0 +1,37 @@
+<?php require_once 'includes/head.php'; 
+
+$blogID = $_GET['blogID'];
+
+
+?>
+<div class="blog">
+<?php
+
+// grab 5 most recent blog posts from the db
+$blogquery = "SELECT id, date,subject,contents FROM blog where id= $blogID"; 
+$blogresult = mysql_query($blogquery);
+//$blogrows = mysql_num_rows($blogresult);
+
+//	$date = mysql_result($blogresult,'date');
+//	$niceDate = prettyDate($date);
+//	$subject = mysql_result($blogresult,'subject');
+//	$contents = mysql_result($blogresult,'contents');
+
+	$blogrow = mysql_fetch_object($blogresult);
+	$blogdate = $blogrow->date;
+	$niceDate = prettyDate($date);
+	$blogsubject = $blogrow->subject;
+	$blogsynopsis = $blogrow->synopsis;
+	$blogcontent = $blogrow->contents;
+
+
+
+
+	
+	echo '<span class="text-blog-header">' . $blogsubject . '</span><br><span class="text-blog-posted"> Posted on </span><span class="text-blog-date">' . $niceDate . "</span><br>";
+	echo '<span class="text-blog"> ' .$blogsynopsis . '<br><br>' .$blogcontents . '</span><br><br>';
+
+ ?>
+</div>
+
+<?php require_once 'includes/footer.php'; ?>
--- index.php
+++ index.php
@@ -30,7 +30,7 @@ for ($i = 0 ; $i < $blogrows ; ++$i) {
 	echo '<span class="text-blog-header">' .$subject . '</span><br><span class="text-blog-posted"> Posted on </span><span class="text-blog-date">' . $niceDate . "</span><br>";
 	echo '<span class="text-blog"> ' .$synopsis . '</span><br>';
 	if ($contents != "NULL") {
-		echo '<span class="text-blog"><a href="blog.php?blogID=$i">Read More</a></span><br><br>';		
+		echo '<span class="text-blog"><a href="blog.php?blogID=' .$i. '">Read More</a></span><br><br>';		
 	}
 } ?>
 </div>
