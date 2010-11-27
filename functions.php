f44cdec93486d75dfebfb963cc78200bc7762bb9
nick <nick@nickwales.co.uk>
2010-11-27 18:25:52 +0000

    added result emailing

--- functions.php
+++ functions.php
@@ -253,7 +253,7 @@ function emailMatchResult($player1,$player2,$p1score,$p2score) {
 	$player2Email = getPlayerEmail($player2);	
 	$to  = 'nick@nickwales.co.uk' . ', '; // note the comma
 	$to .= 'results@tomjohnleague.co.uk';
-	$message = "The result of a match you have played has been added \n " . $player1Name . ": " . $p1score . "\n " . $player2Name . ": " . $p2score . ";
+	$message = "The result of a match you have played has been added \n " . $player1Name . ": " . $p1score . "\n " . $player2Name . ": " . $p2score;
 	$message = wordwrap($message, 70);
 	mail($to, 'TomJohn Result', $message);
 	
