cd1b90a933b5adacbedb45779bc05a291b402aff
nick <nick@nickwales.co.uk>
2011-01-08 21:18:40 +0000

    removed game scores

--- playerdetail.php
+++ playerdetail.php
@@ -44,15 +44,10 @@ for ($i = 0 ; $i < $toPlayRows ; ++$i)
 <span class="text-header">Result History:</span><br>
 <table class="league">
 <tr>
-   <td class="hed">Winner</td>
-   <td class="hed">Loser</td>
-   <td class="hed">Game Score</td>
-   <td class="hed">Game1</td>
-   <td class="hed">Game2</td>
-   <td class="hed">Game3</td>
-   <td class="hed">Game4</td>
-   <td class="hed">Game5</td>
-
+	<td class="hed">Season</td>
+	<td class="hed">Winner</td>
+	<td class="hed">Score</td>
+	<td class="hed">Loser</td>
 </tr>
 <?php
 $query = "select * from results where player1 = $id or player2 = $id";
@@ -61,34 +56,21 @@ $rows = mysql_num_rows($result);
 
 for ($j = 0 ; $j < $rows ; ++$j)
 {
-	$season = mysql_results($result,$j,'seasonID');
+	$season = mysql_result($result,$j,'seasonID');
 	$winner = mysql_result($result,$j,'player1');
 	$loser = mysql_result($result,$j,'player2');
 	$winnerName = getPlayerName($winner);
 	$loserName = getPlayerName($loser);
 	$p1g = mysql_result($result,$j,'player1_score');
 	$p2g = mysql_result($result,$j,'player2_score');
-	$p1g1 = mysql_result($result,$j,'p1g1');
-	$p2g1 = mysql_result($result,$j,'p2g1');
-	$p1g2 = mysql_result($result,$j,'p1g2');
-	$p2g2 = mysql_result($result,$j,'p2g2');
-	$p1g3 = mysql_result($result,$j,'p1g3');
-	$p2g3 = mysql_result($result,$j,'p2g3');
-	$p1g4 = mysql_result($result,$j,'p1g4');
-	$p2g4 = mysql_result($result,$j,'p2g4');
-	$p1g5 = mysql_result($result,$j,'p1g5');
-	$p2g5 = mysql_result($result,$j,'p2g5');
-	
+		
 echo 	'<tr>';
 echo 	'<td class="text-normal">' . $season . '</td>';
 echo 	'<td class="text-normal">' . $winnerName . '</td>';
-echo 	'<td class="text-normal">' . $loserName . '</td>';
 echo 	'<td class="text-normal">' . $p1g . " " . $p2g . '</td>';
-echo 	'<td class="text-normal">' . $p1g1 . " " . $p2g1 . '</td>';
-echo 	'<td class="text-normal">' . $p1g2 . " " . $p2g2 . '</td>';
-echo 	'<td class="text-normal">' . $p1g3 . " " . $p2g3 . '</td>';
-echo 	'<td class="text-normal">' . $p1g4 . " " . $p2g4 . '</td>';
-echo 	'<td class="text-normal">' . $p1g5 . " " . $p2g5 . '</td>';
+echo 	'<td class="text-normal">' . $loserName . '</td>';
+
+
 
 echo	'</tr>';
 }
