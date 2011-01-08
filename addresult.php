5fb7e3b721255ade2e0752783b1426dff5028f5b
nick <nick@nickwales.co.uk>
2011-01-08 19:58:42 +0000

    brought user match result adding into line with admin

--- addresult.php
+++ addresult.php
@@ -15,17 +15,15 @@ if (isset($_POST['p1score'])) $p2score = sanitizeString($_POST['p2score']);
 $p1Elo = getEloRating($player1);
 $p2Elo = getEloRating($player2);
 
-echo $p1Elo . '<br>';
-echo $p2Elo . '<br>';
-
 $duplicate = checkDuplicates($player1,$player2);
 
 if ($duplicate == "yes") {
 	echo "These two have already played this season, try again";
 } else {
-	echo "Your result has been added";	
+	echo "Your result has been added <br><br>";	
 	addMatchResult($seasonID,$player1,$player2,$p1score,$p2score);
-	emailMatchResult($player1,$player2,$p1score,$p2score);	
+	
+	
 	    $elo_calculator = new elo_calculator;
 	if ($p1score == $p2score) {
         $results=$elo_calculator->rating("draw","draw",$p1Elo,$p2Elo);			
@@ -38,11 +36,18 @@ if ($duplicate == "yes") {
         $p1NewEloScore = $results['R3'];
         $p2NewEloScore = $results['R4'];
 
+$player1Name = getPlayerName($player1);
+$player2Name = getPlayerName($player2);
+
+echo $player1Name . "- Ranking points: "  . $p1Elo . ' => ' . $p1NewEloScore . '<br>';
+echo $player2Name . "- Ranking points: "  . $p2Elo . ' => ' . $p2NewEloScore . '<br>';
+
 echo '<br><br>';
 updateEloRating($player1,$p1NewEloScore);
 echo '<br><br>';
 updateEloRating($player2,$p2NewEloScore);
-	
+
+emailMatchResult($player1,$player2,$p1score,$p2score,$p1Elo,$p2Elo,$p1NewEloScore,$p2NewEloScore);	
 }
 
 } else {
@@ -57,65 +62,12 @@ $rows = mysql_num_rows($result);
 
 ?>
 <span class="text-header">Add a result</span><br><br>
-<span class="text-normal"> If you have the game scores, enter them here</span><br><br>
-<form method="post" action="resultadded.php">
-<select name="player1" size="1">
-<option value=Nick Wales>Winner</option>
-<?php 
-for ($j = 0; $j < $rows ; ++$j) {
-	echo '<option value="' . mysql_result($result,$j,'id') . '">Div:' .  mysql_result($result,$j,'number') . " " .  mysql_result($result,$j,'name') . '</option>';
-}
-?>
 
-</select>
 
-<?php 
-for ($i = 1; $i <= 5; ++$i) {
-
-echo '<select name="p1g' . $i . '" size="1">';
-   for ($j = 0 ; $j <= 10; ++$j) {
-	echo '<option value="' . $j . '">' . $j . '</option>';
-   }
-echo '</select>';
-}
-?>
+<!-- Only doing match scores now, game scores look unlikely to be workable -->
 
 
 
-<br>
-<select name="player2" size="1">
-<option value=Nick Wales>Runner Up</option>
-
-
-<?php
-for ($j = 0; $j < $rows ; ++$j) {
-		echo '<option value="' . mysql_result($result,$j,'id') . '">Div:' .  mysql_result($result,$j,'number') . " " .  mysql_result($result,$j,'name') . '</option>';
-//        echo '<option value="' . mysql_result($result,$j,'id') . '">' . mysql_result($result,$j,'name') . '</option>';
-}
-?>
-</select>
-
-<?php 
-for ($k = 1; $k <= 5; ++$k) {
-
-echo '<select name="p1g' . $k . '" size="1">';
-   for ($l = 0 ; $l <= 10; ++$l) {
-	echo '<option value="' . $l . '">' . $l . '</option>';
-   }
-echo '</select>';
-}
-?>
-
-
-
-<input type="hidden" name="points" value="yes">
-<input type="submit" />
-</form>
-
-<!-- and again for those people who can't remember scores -->
-<br><br>
-And again for those people who can't remember their scores / walkovers.
-<br><br>
 
 <form method="post" action="addresult.php">
 <select name="player1" size="1">
@@ -131,7 +83,7 @@ for ($k = 0; $k < $rows ; ++$k) {
 <select name="p1score" size="1">
 <?php 
    for ($k = 1 ; $k <= 3; ++$k) {
-	echo '<option value="' . $k . '">' . $k . '</option>';
+	echo '<option value="' . $k . '" selected="3">' . $k . '</option>';
    }
 ?>
 </select>
@@ -163,6 +115,8 @@ for ($j = 0; $j < $rows ; ++$j) {
 <input type="hidden" name="games" value="yes">
 <input type="submit" />
 </form>
+
+
 
 <?php
  
