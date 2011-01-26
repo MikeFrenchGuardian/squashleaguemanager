d977496340b5bcd10b8b853807e95971b8fe4a49
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2011-01-26 22:24:03 +0000

    fixed for statement

--- trunk/stats.php
+++ trunk/stats.php
@@ -2,12 +2,12 @@
 
 <br><span class="text-header">League Stats</span><br><br>
 
-<span class="text-semibold">Matches Played</span>
+<span class="text-semibold">Matches Played</span><br>
 
 <?php
 $maxSeason = getMaxSeasonID();
 
-for ($i = 0; $i < $maxSeason; ++$i) {
+for ($i = 1; $i < $maxSeason; ++$i) {
 	$matches = seasonMatchCount($i);
 	$revenue = $matches * 4.60;
 	echo 'Season starting ' . getSeasonStart($i) . ' ';
