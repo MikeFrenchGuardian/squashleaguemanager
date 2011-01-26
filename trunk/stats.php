41527e1fb019a285e5a5d307cfbd0e668b23430a
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2011-01-26 21:52:20 +0000

    stats page started off

---
+++ trunk/stats.php
@@ -0,0 +1,20 @@
+<?php require_once 'includes/head.php'; ?>
+
+<br><span class="text-header">League Stats</span><br><br>
+
+<span class="text-semibold">Matches Played</span>
+
+<?php
+$seasonQuery = "select MAX(id) from season";
+$seasonResult = mysql_query($seasonQuery);
+$row = mysql_fetch_object($seasonResult);
+$name = $row->{'MAX(id)'};
+return $name;
+}
+
+echo $name;
+
+?>
+
+<?php require_once 'includes/footer.php'; ?>
+
