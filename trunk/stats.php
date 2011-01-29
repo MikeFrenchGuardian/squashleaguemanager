9026cfa50c2ea52fda4d213a35909f814e5acabc
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2011-01-29 00:38:55 +0000

    matches percentage played...

--- trunk/stats.php
+++ trunk/stats.php
@@ -6,20 +6,36 @@
 
 <?php
 $maxSeason = getMaxSeasonID();
+
+
+
 ?>
 <table class="league">
 	<tr>
 		<td class="hed">Season Start</td>
 		<td class="hed">Matches Played</td>
+		<td class="hed">% Matches Played</td>
 		<td class="hed">Revenue</td>
 	</tr>
-<?php for ($i = 1; $i < $maxSeason; ++$i) {
+<?php 
+for ($i = 1; $i < $maxSeason; ++$i) {
 	$matches = seasonMatchCount($i);
 	$revenue = $matches * 4.60;
+	$divCount = numLeagues($i);
+	
+	for ($j = 1; $j < $divCount; ++$j) {
+			$divSize = $getDivSize();
+			$leagueMatches = ($divSize / 2) * ($divSize -1);
+			$totalMatches = $totalMatches + $leagueMatches;
+	}
+	$matchesPlayed = (($matches/$totalMatches)*100);
+	
+
 
 	echo "<tr>";
 	echo "<td>" . prettyDate(getSeasonStart($i)) . "</td>";
 	echo "<td>" . $matches . "</td>";
+	echo "<td>" . $matchesPlayed . "</td>";
 	echo "<td>&pound;" . $revenue . "</td>";
 	echo "</tr>";
 	echo '<br>';
