094520899ce34c51341520213f94acd5933e7ec2
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2011-02-28 10:02:51 +0000

    grab fname and lname ordered by lname

--- largerLeague-trunk/playerinfo.php
+++ largerLeague-trunk/playerinfo.php
@@ -8,7 +8,7 @@ if ($loggedIn == "true") {
 }
 
 
-$query = "SELECT * FROM player";
+$query = "SELECT * FROM player ORDER by lname";
 $result = mysql_query($query);
 
 $rows = mysql_num_rows($result);
@@ -26,13 +26,14 @@ $rows = mysql_num_rows($result);
 for ($j = 0 ; $j < $rows ; ++$j)
 {
 	$id = mysql_result($result,$j,'id');
-	$name = mysql_result($result,$j,'name');
+	$fname = mysql_result($result,$j,'fname');
+	$lname = mysql_result($result,$j,'lname');
 	$mobilephone = mysql_result($result,$j,'mobilephone');
 	$phone = mysql_result($result,$j,'phone');
 	$email = mysql_result($result,$j,'email');
 	
 echo 	'<tr>';
-echo 	'<td><a class="text-normal" href="playerdetail.php?id=' . $id . '">' . $name . '</td>';
+echo 	'<td><a class="text-normal" href="playerdetail.php?id=' . $id . '">' . $fame . $lname '</td>';
 if ($member == yes){
 	echo 	'<td>' . $mobilephone . '</td>';
 } else {
