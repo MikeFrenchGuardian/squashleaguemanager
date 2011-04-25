0a44ab534b1bc0af43f771b29c945da8e3af17aa
nick <nick@nickwales.co.uk>
2011-04-25 20:26:08 +0000

    added instructions page

---
+++ howto.php
@@ -0,0 +1,12 @@
+<?php require_once 'includes/head.php'; ?>
+
+<br><span class="text-header">Instructions</span><br><br>
+
+Adding a result<br>
+<ul>
+	<li>Successfully logging in with the details provided to you by the league administrator will open up the "Add Result" option<br>
+	<li>Once you've clicked on this, choose the winner and loser and the score then click enter.<br>	
+	<li>You're done, you can now check your league points <a href="/showleague.php">here</a>.<br>
+</ul>
+<?php require_once 'includes/footer.php'; ?>
+
