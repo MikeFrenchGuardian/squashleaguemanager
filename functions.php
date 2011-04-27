52ed28949794ecdbf548623c475a167603a0c500
nick <nick@nickwales.co.uk>
2011-04-27 09:14:19 +0000

    moved function to results page

--- functions.php
+++ functions.php
@@ -791,18 +791,6 @@ function curPageName() {
  return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
 }
 
-function tweeter($message) {
-	$consumerKey    = '1XjVDsxhid6RGC2L87iOw';
-	$consumerSecret = '3D9GIbIEfiKqSMDzHTunAPJ0Cb3jGMpxTGJ5SBKXcZQ';
-	$oAuthToken     = '167934744-nQHj7SI2fmR9kKgp0xPgqxKThzo3b8E5Zm57LtXh';
-	$oAuthSecret    = 'w151Vhz4TQ5cMaCGEeJPZyeHfw13X4PgvIek4UXhzk';
 
-	require_once($_SERVER['DOCUMENT_ROOT'].'../classes/twitter/twitteroauth.php');
-	
-	// create a new instance
-	$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
- 
-	//send a tweet
-	$tweet->post('statuses/update', array('status' => 'Hello World'));
 
 ?>
--- includes/addresult.php
+++ includes/addresult.php
@@ -65,10 +65,22 @@ if ($duplicate == "yes") {
 		
 		$message = "$player1Name just beat $player2Name by $p1score games to $p2score";
 		
-		tweeter($message);
-		
-		
+		function tweeter($message) {
+			$consumerKey    = '1XjVDsxhid6RGC2L87iOw';
+			$consumerSecret = '3D9GIbIEfiKqSMDzHTunAPJ0Cb3jGMpxTGJ5SBKXcZQ';
+			$oAuthToken     = '167934744-nQHj7SI2fmR9kKgp0xPgqxKThzo3b8E5Zm57LtXh';
+			$oAuthSecret    = 'w151Vhz4TQ5cMaCGEeJPZyeHfw13X4PgvIek4UXhzk';
 
+			require_once($_SERVER['DOCUMENT_ROOT'].'classes/twitter/twitteroauth.php');
+	
+			// create a new instance
+			$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
+ 
+			//send a tweet
+			$tweet->post('statuses/update', array('status' => 'Hello World'));
+		}
+
+		tweeter($message);
 		
 		}
 	}
