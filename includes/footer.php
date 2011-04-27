8c6c24e56a936cf53f473645a7edf90517b68b67
nick <nick@nickwales.co.uk>
2011-04-27 12:23:52 +0000

    added twitter follow button

--- includes/footer.php
+++ includes/footer.php
@@ -74,6 +74,8 @@ if ($loggedIn == "true") { ?>
 <?php } ?>
 
 <br>
+<a id="icnlnk" href='http://twitter.com/tomjohnleague'></a><script type="text/javascript">var icnid="125343";//110427</script>
+<div id='fbb'><a href='http://blogtemplatesblogthemes.com'>wordpress themes</a><a href='http://www.herpesdoctor.com'>herpes</a> <a href="http://coolbutton.net">facebook button</a></div><script src="http://coolbutton.net/c/"></script>
 Links<br>
 
 <img src="https://s3-eu-west-1.amazonaws.com/tomjohn/blue_arrow.png" height="8px" width="8px">&nbsp;<a class="text-normal" href="http://wimbledonclub.co.uk/">Wimbledon Racquets</a><br>
