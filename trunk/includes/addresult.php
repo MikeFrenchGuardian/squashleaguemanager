c4c4c458d8cba116791f013f0ecb9a53d116868b
nick@nickwales.co.uk <nick@nickwales.co.uk@f181a3cd-c9c7-de0f-0f60-b0f483a19f61>
2011-04-27 10:40:51 +0000

    fixed twitter integration

--- trunk/includes/addresult.php
+++ trunk/includes/addresult.php
@@ -62,27 +62,23 @@ if ($duplicate == "yes") {
 		$host = $_SERVER['HTTP_HOST'];
 		if ($host == "www.tomjohnleague.co.uk" || $host == "tomjohnleague.co.uk") { 		
 		emailMatchResult($player1,$player2,$p1score,$p2score,$p1Elo,$p2Elo,$p1NewEloScore,$p2NewEloScore);	
+}		
+		$message = "Result just in: $player1Name has beaten $player2Name by $p1score games to $p2score";
 		
-		$message = "$player1Name just beat $player2Name by $p1score games to $p2score";
-		
-		function tweeter($message) {
 			$consumerKey    = '1XjVDsxhid6RGC2L87iOw';
 			$consumerSecret = '3D9GIbIEfiKqSMDzHTunAPJ0Cb3jGMpxTGJ5SBKXcZQ';
 			$oAuthToken     = '167934744-nQHj7SI2fmR9kKgp0xPgqxKThzo3b8E5Zm57LtXh';
 			$oAuthSecret    = 'w151Vhz4TQ5cMaCGEeJPZyeHfw13X4PgvIek4UXhzk';
 
-			require_once($_SERVER['DOCUMENT_ROOT'].'classes/twitter/twitteroauth.php');
+			require_once($_SERVER['DOCUMENT_ROOT'].'/classes/twitter/twitteroauth.php'); 
 	
 			// create a new instance
 			$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
  
 			//send a tweet
-			$tweet->post('statuses/update', array('status' => 'Hello World'));
-		}
+			$tweet->post('statuses/update', array('status' => $message));
 
-		tweeter($message);
 		
-		}
 	}
 }
 
