71c6f1017a9c5ce31621f40bf99d26fe061257be
nick <nick@nickwales.co.uk>
2011-05-26 21:57:00 +0000

    missed some other shit

--- admin/editresults.php
+++ admin/editresults.php
@@ -9,7 +9,7 @@ if (isset($_GET["delete"])) {
 
 	echo 'Are you sure you want to delete this result?<br><br>';
 	
-	echo '<a href="/admin/editresults.php?deleted=yes&"';
+	echo '<a href="/admin/editresults.php?deleted=yes&result=' . $result . '">';
 	
 	
 } else if (isset($_GET["deleted"])) {
